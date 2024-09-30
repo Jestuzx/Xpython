ee827e70d9cc0ecec3f2f71cfdc907f4bf2bc042
Jestuzx <ycheba38@gmail.com>
2024-09-30 14:25:49 +0200

    add(app): shake effect for washmachine

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from re import purge
-
+import random
 import pygame
 from sys import exit
 
@@ -16,21 +16,35 @@ text_surface = test_font.render('XPython task',False, 'Black')
 
 
 washing_machine = pygame.image.load('../img/pixil-frame-0.png')
-washing_machine = pygame.transform.scale(washing_machine,(100,100))
+washing_machine_rect = washing_machine.get_rect(center=(400,220))
+washing_machine_y_pos = 160
 
 while True:
     # draw all elements
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    for _ in range(10):  # Кількість рухів для тряски
+        # Випадкові зміщення
+        offset_x = random.randint(-5, 5)  # Випадкове зміщення по X
+        offset_y = random.randint(-5, 5)  # Випадкове зміщення по Y
+
+        # Переміщення об'єкта
+        screen.blit(washing_machine, (washing_machine_rect.x + offset_x, washing_machine_rect.y + offset_y))
+
+        # Оновлення екрану
+        pygame.display.flip()
+        pygame.time.delay(20)
 
     screen.blit(ground_surface,(0,260))
     screen.blit(sky_surface,(0,-40))
     screen.blit(text_surface, (300,50))
-    screen.blit(washing_machine,(350,160))
+    #screen.blit(washing_machine,(350,160))
 
     pygame.display.update()
     clock.tick(60)
+
