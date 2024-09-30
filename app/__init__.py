0863c774454954ec4aa4a9507278e24dc8a4e6e2
Jestuzx <ycheba38@gmail.com>
2024-09-30 12:56:37 +0200

    Add(): add background img

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 import pygame
 from sys import exit
-
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Xpytask1')
 clock = pygame.time.Clock()
+
+
+ground_surface = pygame.image.load('img/ground.png')
+sky_surface = pygame.image.load('img/sky.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+
+    screen.blit(ground_surface,(0,260))
+    screen.blit(sky_surface,(0,-40))
     # draw all elements
     pygame.display.update()
     clock.tick(60)
