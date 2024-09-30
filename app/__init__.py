0dfb2d55955ccf816500dbf4eb4780b0bf13bf92
Jestuzx <ycheba38@gmail.com>
2024-09-30 13:46:20 +0200

    add washingmachine

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,26 @@
-from imghdr import test_exr
+from re import purge
 
 import pygame
 from sys import exit
+
 pygame.init()
+
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Xpytask1')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('pixeltype.ttf', 50)
+test_font = pygame.font.Font('../font/pixeltype.ttf', 50)
 
-ground_surface = pygame.image.load('img/ground.png')
-sky_surface = pygame.image.load('img/sky.png')
+ground_surface = pygame.image.load('../img/ground.png')
+sky_surface = pygame.image.load('../img/sky.png')
 text_surface = test_font.render('XPython task',False, 'Black')
-mainobject_surface = pygame.image.load('/img/mainobject.png')
 
+
+washing_machine = pygame.image.load('../img/pixil-frame-0.png')
+washing_machine = pygame.transform.scale(washing_machine,(100,100))
 
 while True:
     # draw all elements
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -25,7 +30,7 @@ while True:
     screen.blit(ground_surface,(0,260))
     screen.blit(sky_surface,(0,-40))
     screen.blit(text_surface, (300,50))
-    screen.blit(mainobject_surface,(0,0))
+    screen.blit(washing_machine,(350,160))
 
     pygame.display.update()
     clock.tick(60)
